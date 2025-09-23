106ee90fe321989ba7e4c7466c63694f5f30815a
rick137-dev <caiulorick137@gmail.com>
2025-09-23 15:06:04 +0200

    Added train method to Tabular Q learning agent, modified act slightly to increase readability

--- TabularQAgent.py
+++ TabularQAgent.py
@@ -66,6 +66,10 @@ class TabularQLearning(Agent):
             filename = os.path.join(self.TABULAR_Q_CHECKPOINT, Q_file)
             self.Q = load(filename)
 
+
+    def max_Q(self, state):
+        return np.max(self.Q[state])
+
     def evaluate(self,seed = None):
         total_return = float(0)
         original_flag = self.training_flag
@@ -89,31 +93,60 @@ class TabularQLearning(Agent):
     def train(self,training_episodes, checkpoint_iteration, env, print_statement = False):
         original_flag = self.training_flag
         self.set_training_flag(True)
-
+        max_evaluation,_ = self.evaluate()
+        episode_rewards = []
+        evaluation_rewards = []
 
         for iteration in range(training_episodes):
-            return_dict = env.record_episode(agent = self)
-            envs = return_dict["total_environment_history"]
+            truncated = False
+            intersected = False
+            env.reset()
+            episode_reward = float(0)
 
+            while truncated is False and intersected is False:
+                state = self.observe(env)
+                action = self.act(env)
+                step_reward, _, truncated, intersected, _ = env.step(action)
+                episode_reward += step_reward
+
+                next_state = self.observe(env)
+                if truncated or intersected:
+                    self.Q[state, action] = self.Q[state, action] + self.learning_rate * (
+                                step_reward  - self.Q[state, action])
+                else:
+                    self.Q[state, action] = self.Q[state, action] + self.learning_rate * (
+                                step_reward + self.discount_factor * self.max_Q(next_state) - self.Q[state, action])
+
+            episode_rewards.append(episode_reward)
             if (iteration+1)%checkpoint_iteration ==0:
-                print("")
+                if print_statement:
+                    print("The current iteration is "+str(iteration) + " and the current episode reward is " + str(episode_reward) +" and the best evaluation rewward seen is "+str(max_evaluation))
+
+                current_eval,_ = self.evaluate()
+                evaluation_rewards.append(current_eval)
+                if current_eval > max_evaluation:
+                    self.Q_Version += 1
+                    self.save_Q_table()
+                    max_evaluation = current_eval
 
 
         self.set_training_flag(original_flag)
+        return episode_rewards , evaluation_rewards , max_evaluation , self.learning_rate , self.discount_factor , self.evaluation_episode_max_length , self.evaluation_episodes
 
     def observe(self, env):
         return Observer.encode_1024(env)
 
     def act(self,env):
         state = self.observe(env)
-        action_probabilities = self.Q[state]
+        state_Q_values = self.Q[state]
+
         if self.training_flag:
             p = [self.epsilon, 1-self.epsilon]
             random_action_choice = self.train_rng.choice(len(p),p=p)
             if random_action_choice ==0:
                 return self.act_rng.integers(NUM_ACTIONS)
             else:
-                return np.argmax(action_probabilities)
+                return np.argmax(state_Q_values)
 
         else:
-            return np.argmax(action_probabilities)
+            return np.argmax(state_Q_values)
