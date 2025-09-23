ff93f27e891ab0eac43895d96e8d35f31ce02a3b
rick137-dev <caiulorick137@gmail.com>
2025-09-23 16:45:34 +0200

    Small edits

--- main.py
+++ main.py
@@ -4,9 +4,5 @@ from gameEnv import *
 
 
 env = SnakeEnvironment(step_limit=4000,step_reward=-0.01)
-agent = TabularQAgent(learning_rate = 0.05, discount_factor = 0.999, evaluation_episodes = 200,evaluation_episode_max_length = 2000)
 
-
-episode = env.record_episode(agent)
-anim = Visualizer.visualize_episode(episode,save_local=False)
 
