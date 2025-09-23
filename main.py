92f3b4135ac0c0d0cc3f82fdf2e80a8563c0c74c
rick137-dev <caiulorick137@gmail.com>
2025-09-23 16:35:32 +0200

    Small edits to main to train Q Agent and show episode recording animation

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from TabularReinforceAgent import TabularReinforceAgent
+from Agents.TabularQAgent import TabularQAgent
 from Visualize import *
 from gameEnv import *
 
 
 env = SnakeEnvironment(step_limit=4000,step_reward=-0.01)
-agent = TabularReinforceAgent(evaluation_episode_max_length = 4000, learning_rate=0.02, discount_factor=1)
+agent = TabularQAgent(learning_rate = 0.05, discount_factor = 0.999, evaluation_episodes = 200,evaluation_episode_max_length = 2000)
+
 
 episode = env.record_episode(agent)
-Visualizer.visualize_episode(episode)
+anim = Visualizer.visualize_episode(episode,save_local=False)
+
